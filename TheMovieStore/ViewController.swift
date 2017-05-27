4e2bd78bb82a8f27a5e240228d49d0b1e36c7e36
Lucas <hoangyendinh0494@gmail.com>
2017-05-27 16:29:25 +0700

    Initial Commit

---
+++ TheMovieStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheMovieStore
+//
+//  Created by Cntt23 on 5/27/17.
+//  Copyright © 2017 ThanhKhoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
